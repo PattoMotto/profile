38fea058242f82a2caae0e47c1519dd5969e9315
Patompong Manprasatkul <neungnarakjung@gmail.com>
2017-03-21 00:12:56 +0700

    Add gen file Update json file

---
+++ gen.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+### prerequisite ###
+# sudo npm install resume-cli -g
+# sudo npm install jsonresume-theme-slick -g
+# sudo npm install html-minifier -g
+### prerequisite ###
+
+resume export gen.html --theme slick
+sed -i '' 's/>pat@/>pat[at]/' gen.html
+html-minifier --case-sensitive --html5 --remove-comments --remove-tag-whitespace --minify-css --collapse-whitespace --sort-attributes --sort-class-name gen.html -o index.html
+rm gen.html
