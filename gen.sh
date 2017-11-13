4711cf696e2b471b658be104b9b030cecd95d510
Patompong Manprasatkul <neungnarakjung@gmail.com>
2017-11-13 09:33:43 +0700

    update resume and theme

--- gen.sh
+++ gen.sh
@@ -6,7 +6,7 @@
 # sudo npm install html-minifier -g
 ### prerequisite ###
 
-resume export gen.html --theme slick
+resume export gen.html --theme kendall
 sed -i '' 's/>pat@/>pat[at]/' gen.html
 sed -i '' 's#http://#https://#g' gen.html
 html-minifier --case-sensitive --html5 --remove-comments --remove-tag-whitespace --minify-css --collapse-whitespace --sort-attributes --sort-class-name gen.html -o index.html
