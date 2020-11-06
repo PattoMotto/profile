14ab20d78d91cd02ab58e33bd583681f4cecd81a
Patompong Manprasatkul <1745000+PattoMotto@users.noreply.github.com>
2020-11-06 12:15:11 +0400

    Update profile

--- gen.sh
+++ gen.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
 ### prerequisite ###
-# sudo npm install resume-cli -g
-# sudo npm install jsonresume-theme-kendall -g
-# sudo npm install replace -g
-# sudo npm install html-minifier -g
+# npm install -g resume-cli jsonresume-theme-kendall replace html-minifier
 ### prerequisite ###
 
+# THEME https://jsonresume.org/themes/
 # https://favicon.io/favicon-generator/
 
 resume export gen.html --theme kendall
 replace "<head>" "<head><link rel='shortcut icon' type='image/x-icon' href='favicon.ico'/>" gen.html
 replace "http://" "https://" gen.html
-replace "manprasatkul@" "manprasatkul[at]" gen.html
+# replace "manprasatkul@" "manprasatkul[AT]" gen.html
+# replace "@manprasatkul" "[AT]manprasatkul" gen.html
 html-minifier --case-sensitive --html5 --remove-comments --remove-tag-whitespace --minify-css --collapse-whitespace --sort-attributes --sort-class-name gen.html -o index.html
 rm gen.html
